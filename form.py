5b73d71976bfc1d9d10d33b52b1af6f634b7739d
shevtsovsv <36815732+shevtsovsv@users.noreply.github.com>
2020-03-18 10:47:34 +0300

    Create form.py

---
+++ form.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+from flask import Flask
+from flask import render_template
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/index')
+def index():
+    user = {'username': 'Эльдар Рязанов'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        },
+        {
+            'author': {'username': 'Ипполит'},
+            'body': 'Какая гадость эта ваша заливная рыба!!'
+        }
+    ]
+    return render_template('index.html', user=user, posts=posts)
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
